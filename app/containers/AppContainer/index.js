f69d5011e7077f6e01c74a7a319f7120adffb4dc
Nadia Dillon <dillon.nadia@gmail.com>
2017-03-02 11:58:35 +0100

    add basic routing/ navigation

--- app/containers/AppContainer/index.js
+++ app/containers/AppContainer/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import SurveyContainer from '../SurveyContainer/'
+import { Scene, Router } from 'react-native-router-flux';
+import SurveyContainer from '../SurveyContainer/';
 
 export default class AppContainer extends Component {
   render() {
     return (
-
-      <SurveyContainer />
+      <Router>
+        <Scene key="root">
+          <Scene key="survey" component={ SurveyContainer }  hideNavBar={true}/>
+        </Scene>
+      </Router>
     );
   }
 }
