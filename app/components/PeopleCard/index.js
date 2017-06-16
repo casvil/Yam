33ae40b48a46182c1a123e69b1a836ffe4f62786
David Casanellas <davidcasanell@gmail.com>
2017-06-16 19:20:16 +0200

    feat: added picker in people card

--- app/components/PeopleCard/index.js
+++ app/components/PeopleCard/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { View , Card, Image, Title, Button, NavigationBar } from '@shoutem/ui';
-import { Text, Slider } from 'react-native'
+import { Text, Slider, Picker } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import styles from './styles'
@@ -9,6 +9,9 @@ import styles from './styles'
 export default class PeopleCard extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+
+    }
   }
 
 
@@ -23,15 +26,16 @@ export default class PeopleCard extends Component {
         styleName="large-square"
         source={{uri: 'https://images.unsplash.com/photo-1493274366568-b608e3895174?dpr=2&auto=format&fit=crop&w=1500&h=1060&q=80&cs=tinysrgb&crop=&bg='}}
         />
-        <Slider
-        value={ this.props.value }
-        minimumValue={ 1 }
-        maximumValue={ 6 }
-        step={ 1 }
-        onValueChange={(value) => this.props.handleChange(value)}
-        minimumTrackTintColor={ '#70C1B3' }
-        maximumTrackTintColor={ '#C2C2C2' }
-        />
+        <Picker
+          selectedValue={this.props.value}
+          onValueChange={(value) => this.props.handleChange(value)}>
+          <Picker.Item label="1" value="1" />
+          <Picker.Item label="2" value="2" />
+          <Picker.Item label="3" value="3" />
+          <Picker.Item label="4" value="4" />
+          <Picker.Item label="5" value="5" />
+          <Picker.Item label="6" value="6" />
+        </Picker>
         <View styleName = "horizontal h-center v-center">
           <Icon size={30} name="person" color="#70C1B3" />
           <Text style={styles.people}>{this.props.value} people</Text>
